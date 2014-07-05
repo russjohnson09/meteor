3d1264a517c2ca2d72dc4638336e76820100f226
russ <russjohnson09@gmail.com>
2014-07-05 18:29:09 -0500

    user list

--- task-man/task-man.js
+++ task-man/task-man.js
@@ -7,18 +7,32 @@ if (settings) {
 }
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to task-man.";
-  };
+
+    Template.userlist.users = function() {
+        return Users.find({});
+    };
+    
+    Template.usersum.tasks = function(uid) {
+        //console.log(uid);
+       // var c = Tasks.find({uid: uid});
+        var tasks = {};
+        tasks.count = 1;
+        tasks.complete = 1;
+        return tasks;
+    };
   
   Template.tasks.tasks = function () {
     return Tasks.find({});
   };
   
-  Template.task.assignedUser = function(id) {
-    console.log(id);
-    var user = Users.findOne({taskId: id});
-    console.log(user);
+  Template.userinput.users = function() {
+    return Users.find({});
+  };
+  
+  Template.task.assignedUser = function(uid) {
+    //console.log(id);
+    var user = Users.findOne({_id: uid});
+    //console.log(user);
     if (user) {
         return user.name;
     }
@@ -27,16 +41,28 @@ if (Meteor.isClient) {
   
   Template.addtask.events({
     'click #addtask': function (e) {
-            var name = document.getElementById("taskName").value;
-            console.log(name);
-            Tasks.insert({name: name});
+            var uid;
+            var name = document.getElementById("taskname").value;
+            var userName = document.getElementById("user").value;
+            console.log(userName);
+            if (userName.length > 0) {
+                var user = Users.findOne({name: userName});
+                if (user) {
+                    console.log(user);
+                    uid = user._id;
+                }
+                else {
+                    uid = Users.insert({name: userName});
+                }
+            }
+            Tasks.insert({name: name, uid: uid});
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    clear();
+    //clear();
     if (Tasks.find({}).count() === 0) {
         addTasks();
     }
@@ -52,8 +78,7 @@ function addTasks() {
     var taskNames = ['food','work','play'];
     var users = ['mark','susan','matt'];
     for (var i=0; i < taskNames.length; i++) {        
-        var taskId = Tasks.insert({name: taskNames[i]});
-        console.log(taskId);
-        var uid = Users.insert({name:users[i], taskId:taskId});
+        var uid = Users.insert({name:users[i]});
+        var taskId = Tasks.insert({name: taskNames[i], uid:uid});
     }
 };
